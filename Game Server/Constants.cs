0b37a482683ef2681f7be656604580f84a2f2c59
Nooruu <cyb03gn1nja@gmail.com>
2021-04-30 00:55:49 -0700

    Added Game Server
    TCP protocol for transferring packets between server and client. Right now the IP of the server is set to the machine's local IP.

---
+++ Game Server/Constants.cs
@@ -0,0 +1,8 @@
+namespace Game_Server
+{
+    public class Constants
+    {
+        public const int TICKS_PER_SEC = 30;
+        public const int MS_PER_TICK = 1000 / TICKS_PER_SEC;
+    }
+}
---
+++ Game Server/GameLogic.cs
@@ -0,0 +1,10 @@
+namespace Game_Server
+{
+    public class GameLogic
+    {
+        public static void Update()
+        {
+            ThreadManager.UpdateMain();
+        }
+    }
+}
